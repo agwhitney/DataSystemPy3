7baa5b5760e30f170940a429d423f7dd11e8409f
Adam Whitney <ag.whitney2@gmail.com>
2026-08-03 14:31:58 -0600

    Successful MQTT connection.

--- hammr/telemetryhandler.py
+++ hammr/telemetryhandler.py
@@ -5,6 +5,21 @@ Include the handler into `instruments.SerialTransportThermistors`.
 """
 
 
+from dotenv import load_dotenv
+import os
+import paho.mqtt.client as mqtt
+import time
+
+
+load_dotenv()
+BROKER = os.getenv("MQTT_BROKER", "")
+USERNAME = os.getenv("MQTT_USERNAME")
+PASSWORD = os.getenv("MQTT_PASSWORD")
+PORT = os.getenv("MQTT_PORT")
+CERTIFICATE = os.getenv("MQTT_CERT")
+topic = "python/mqtt"
+
+
 class ThermistorTelemetryHandler:
     def __init__(self):
         self.hot_threshold = 0.321  # lower voltage is hotter. 0.321 V = 50 *C
@@ -15,3 +30,46 @@ class ThermistorTelemetryHandler:
             0.321, 0.321, 0.321, 0.321, 0.321, 0.321, 0.321, 0.321,
             0.321, 0.321, 0.321, 0.321, 0.321, 0.321, 0.321, 0.321,
         ]
+
+
+    def connect_mqtt(self, broker, port, username, password):
+        def on_connect(client, userdata, flags, reason_code, properties):
+            if reason_code == 0:
+                print("Connected")
+            else:
+                print(f"Connection failed, return code {reason_code}")
+
+        client = mqtt.Client(
+            client_id='csu',
+            callback_api_version=mqtt.CallbackAPIVersion.VERSION2,
+        )
+
+        client.on_connect = on_connect
+        client.username_pw_set(USERNAME, PASSWORD)
+        client.tls_set(CERTIFICATE)
+        client.connect(BROKER, int(PORT))
+        return client
+
+
+    def publish(self, client, topic):
+        msg_count = 1
+        while True:
+            time.sleep(1)
+            msg = f"messages: {msg_count}"
+            result = client.publish(topic, msg)
+            status = result[0]
+            if status == 0:
+                print(f"Sent `{msg}` to topic `{topic}`")
+            else:
+                print(f"Failed to send message to topic `{topic}`")
+            msg_count += 1
+            if msg_count > 5:
+                break
+
+
+    def run(self):
+        cl = self.connect_mqtt(BROKER, PORT, USERNAME, PASSWORD)
+        cl.loop_start()
+        self.publish(cl, 'python/mqtt')
+        cl.loop_stop()
+        cl.disconnect()
